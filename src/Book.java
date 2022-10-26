316d28cebd1ac4867932c4982773da145e04df26
HeOleg <lu4shemeb@gmao;.com>
2022-10-26 13:09:47 +0300

    Сделал дз

---
+++ src/Book.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Book {
+    private final String name;
+    private final Author author;
+    private Integer publicationYear;
+
+    public Book(String name, Author author, Integer publicationYear) {
+        this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear=publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(name, book.name) && Objects.equals(author, book.author) && Objects.equals(publicationYear, book.publicationYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, publicationYear);
+    }
+}
